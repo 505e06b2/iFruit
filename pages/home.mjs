1775d16fce9ef198ffd8f56a3ad46233717d492d
Jake <505e06b2@gmail.com>
2021-06-06 21:50:10 +0100

    sorted

--- pages/home.mjs
+++ pages/home.mjs
@@ -1,12 +1,10 @@
 function setHeaderValues() {
 	const timedisplay = document.getElementById("home_timedisplay");
-	const weekday = document.getElementById("home_weekday");
-	if(timedisplay && weekday) {
+	if(timedisplay) {
 		const t = new GTATime();
 		const hour = t.hour.toString().padStart(2, "0");
 		const min = t.minute.toString().padStart(2, "0");
 		timedisplay.innerHTML = `${hour}:${min}`;
-		weekday.innerHTML = t.weekday;
 	}
 }
 
@@ -20,10 +18,7 @@ setInterval(setHeaderValues, 500);
 ex.innerHTML = `
 	<div class="header_spacing"></div>
 	<div class="header">
-		<div>
-			<div id="home_timedisplay">&nbsp;</div>
-			<div id="home_weekday">&nbsp;</div>
-		</div>
+		<div id="home_timedisplay">&nbsp;</div>
 	</div>
 	<div class="footer_spacing"></div>
 	<div class="footer">#</div>
